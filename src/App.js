262b0f8ba4d0916fc8d32578f2b9517fa8f9947a
SumaiaSorna <sorna.sumaia@gmail.com>
2022-02-17 11:07:18 +0000

    create grid for the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Container, Grid } from "@mui/material";
+
 import "./App.css";
 
 export const App = () => {
-  return <h1>hi from sumaia</h1>;
+  return (
+    <Container>
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={3}
+          style={{ backgroundColor: "blue" }}
+        >
+          <Profile />
+        </Grid>
+        <Grid item xs style={{ backgroundColor: "red" }}>
+          <Header />
+          <Projects />
+          <Resume />
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };

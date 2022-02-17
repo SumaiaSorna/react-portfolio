75bbc20c7aa7a996979e7da263615c49fd7e5792
SumaiaSorna <sorna.sumaia@gmail.com>
2022-02-17 15:17:42 +0000

    basic layout for the profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,23 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 export const Profile = () => {
-  return <div> this is profile components</div>;
+  return (
+    <div className="profile container-shadow">
+      <div className="profile-name">
+        <Typography className="name">Sumaia Salahuddin</Typography>
+        <Typography className="title">Full Stack Web Developer</Typography>
+      </div>
+
+      <figure className="profile-image">
+        <img src="" alt="" />
+      </figure>
+
+      <div className="profile-info">
+        Insert Timeline
+        <br />
+        <button>My Button</button>
+      </div>
+    </div>
+  );
 };

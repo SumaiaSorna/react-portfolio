95f15777c6e9b5c9c07a422671051960a8963233
SumaiaSorna <sorna.sumaia@gmail.com>
2022-02-20 21:55:03 +0000

    refactor and styles navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,64 +1,68 @@
 import React from "react";
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
+//import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+import "./Header.css";
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+// function TabPanel(props) {
+//   const { children, value, index, ...other } = props;
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box sx={{ p: 3 }}>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// }
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.number.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
+
+// function a11yProps(index) {
+//   return {
+//     id: `simple-tab-${index}`,
+//     "aria-controls": `simple-tabpanel-${index}`,
+//   };
+// }
 
 export const Header = () => {
-  const [value, setValue] = React.useState(0);
+  // const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue);
+  // };
 
   return (
-    <div>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+    <div className="navbar" sticky="top">
+      <Box>
         <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
+
+        // value={value}
+        // onChange={handleChange}
+        //aria-label="basic tabs example"
         >
-          <Tab label="About Me" {...a11yProps(0)} />
-          <Tab label="Projects" {...a11yProps(1)} />
-          {/* <Tab label="Item Three" {...a11yProps(2)} /> */}
+          <Tab label="About Me" />
+          <Tab label="Projects" />
+          <Tab label="Contact Me" />
         </Tabs>
       </Box>
-      <TabPanel value={value} index={0}>
+      {/* <TabPanel value={value} index={0}>
+        {" "}
         Item One
       </TabPanel>
       <TabPanel value={value} index={1}>
@@ -66,7 +70,7 @@ export const Header = () => {
       </TabPanel>
       <TabPanel value={value} index={2}>
         Item Three
-      </TabPanel>
+      </TabPanel> */}
     </div>
   );
 };

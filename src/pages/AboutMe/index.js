44f08ba5f245ff2e7ead82dc97f6d81ccc411732
SumaiaSorna <sorna.sumaia@gmail.com>
2022-02-20 11:21:55 +0000

    stylles skills section

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Paper } from "@mui/material";
 import "./AboutMe.css";
 
 const profileData = require("../../utils/profileData.json");
@@ -15,26 +15,34 @@ export const AboutMe = () => {
           <h6 className="section-title">About Me</h6>
         </Grid>
         <Grid item xs={12}>
-          <Typography className="section-text">
-            Sumaia is a full-stack web developer in training and have the
-            necessary education, training and enthusiasm to make a difference in
-            a working environment. Sumaia was a marketing graduate but retrained
-            herself in web development, and thus poses a combined skill from
-            both disciplines for the digital world.
-          </Typography>
+          <Paper elevation={0}>
+            <Typography className="section-text">
+              Sumaia is a full-stack web developer in training and have the
+              necessary education, training and enthusiasm to make a difference
+              in a working environment. Sumaia was a marketing graduate but
+              retrained herself in web development, and thus poses a combined
+              skill from both disciplines for the digital world.
+            </Typography>
+          </Paper>
         </Grid>
       </Grid>
 
       {/* Skills */}
       <Grid container className="section">
+        <Grid xs={12}>
+          <span></span>
+          <h6 className="section-title">Skills</h6>
+        </Grid>
         {profileData.map((skill) => (
           <Grid item xs={12} sm={6} md={3}>
-            <Typography variant="h6" className="skills-title">
-              {skill.title}
-            </Typography>
-            {skill.description.map((element) => (
-              <Typography>{element}</Typography>
-            ))}
+            <Paper elevation={0}>
+              <Typography variant="h6" className="skills-title">
+                {skill.title}
+              </Typography>
+              {skill.description.map((element) => (
+                <Typography variant="h6">{element}</Typography>
+              ))}
+            </Paper>
           </Grid>
         ))}
       </Grid>

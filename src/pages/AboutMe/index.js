29584fb250080ece467f15ea5a75bce76ee80094
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-03 09:01:17 +0000

    remove unused code

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -51,35 +51,6 @@ export const AboutMe = () => {
           ))}
         </Grid>
       </Grid>
-
-      {/* <Grid container className="section">
-        <Grid item xs={12} lg={7}>
-          <Grid container>
-            <Grid item pb={6}>
-              <span></span>
-              <h6 className="section-title">Contact Form</h6>
-            </Grid>
-
-            <Grid item xs={12}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                  <TextField fullWidth name="name" label="Name" />
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  <TextField fullWidth name="email" label="Email" />
-                </Grid>
-
-                <Grid item xs={12}>
-                  <TextField fullWidth name="message" label="Message" />
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-
-        <Grid item xs={12} lg={5}></Grid>
-      </Grid> */}
     </div>
   );
 };
